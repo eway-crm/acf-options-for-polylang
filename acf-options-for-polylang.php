dff4c9ff03b4660de63432f291cfe2438c3af963
Nicolas Kulka <kulka.nicolas@gmail.com>
2016-10-05 15:41:17 +0200

    Update acf-options-for-polylang.php

--- acf-options-for-polylang.php
+++ acf-options-for-polylang.php
@@ -1,12 +1,33 @@
 <?php
 /*
-  Plugin Name: BEA - Polylang's ACF Option page
-  Plugin URI: http://www.beapi.fr
-  Description: Add ACF options page support for Polylang
-  Author: BeAPI
-  Author URI: http://www.beapi.fr
-  Version: 1.0.1
+ Plugin Name: BEA - Polylang's ACF Option page
+ Version: 1.0.1
+ Plugin URI: http://www.beapi.fr
+ Description: Add ACF options page support for Polylang
+ Author: BE API Technical team
+ Author URI: http://www.beapi.fr
+ Domain Path: languages
+ Text Domain: acf-options-for-polylang
+ 
+ ----
+ 
+ Copyright 2016 BE API Technical team (human@beapi.fr)
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
 class BEA_ACF_For_Polylang {
 
 	function __construct() {
