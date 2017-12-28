efbc9c0e83fd312c3f2951ca7a0ae30683c1630d
Maxime Culea <culea.maxime@gmail.com>
2017-12-28 11:21:39 +0100

    #10 : change post id with current lang only for custom page id

--- acf-options-for-polylang.php
+++ acf-options-for-polylang.php
@@ -189,7 +189,9 @@ class BEA_ACF_For_Polylang {
 	 * @return string
 	 */
 	function set_options_id_lang( $future_post_id, $original_post_id ) {
-		if ( ! $this->is_option_page( $original_post_id ) ) {
+		// Only on custom post id option page
+		$options_pages = $this->get_option_page_ids();
+		if ( empty( $options_pages ) || ! in_array( $original_post_id, $options_pages ) ) {
 			return $future_post_id;
 		}
 
